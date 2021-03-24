f12a599eb490533697f6de8ceac036f46aa07b85
Mahi Nair <mahifreya@gmail.com>
2021-03-24 00:04:42 -0400

    a1 index.ts

--- index.ts
+++ index.ts
@@ -1,83 +1,41 @@
+
 //add express
-import express from 'express';
+const express = require('express');
 
 //initialize express
 const app = express();
 
-//add firebase
-var admin = require("firebase-admin");
+//make the GET request
+app.get('/book', function(req, res){
 
-//my own database, commented out in the submission
-var serviceAccount = require("./service-account.json");
-
-/*admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount)
-});
-*/
-
-//initialize databse 
-const db = admin.firestore();
-
-const port = 8080;
-
-//add bodyParser
-const bodyParser = require('body-parser');
-
-//use bodyParser in the requests
-app.use(bodyParser.json());
-
-// create a song type and song with id
-type Song = {
-    arist: string;
-    name: string;
-    rating: number;
-  };
-  
-  type SongWithID = Song & {
-    id: string;
-  };
-
-//songs collection from firestore db
-const songs = db.collection('Songs');
-
-//STEP 1: Make the POST request
-app.post('/createSong', async function(req: { body: Song; }, res: { send: (arg0: any) => void; }){
-    const song: Song = req.body;
-    const songDoc = songs.doc();
-    await songDoc.set(song);
-    res.send(songDoc.id);
-
-});
-
-//STEP 2: Make the GET request
-
-app.get('/getSongs', async function(_: any, res: { send: (arg0: SongWithID[]) => void; }) {
-    const songsSnapshot = await songs.orderBy('name', 'asc').get();
-    const allSongsDoc = songsSnapshot.docs;
-    const songs1: SongWithID[] = [];
-    for (let doc of allSongsDoc) {
-        const song: SongWithID = doc.data() as SongWithID;
-        song.id = doc.id;
-        songs1.push(song);
+    //make the GET request w/ query
+    
+    if (req.query.name === 'peter'){
+        res.send('Cracking the Coding Interview');
     }
-    res.send(songs1);
-})
+    
+    if (req.query.name === 'becky'){
+        res.send('The Pragmatic Programmer');
+    }
 
-app.listen(port, () => console.log('App started!'));
-
-//STEP 3: Update a Songs rating (POST)
-
-app.post('/updateRating', async function(req: { body: { id: any; rating: number; }; }, res: { send: (arg0: any) => void; }) {
-    const id = req.body.id;
-    const rating = req.body.rating;
-    await songs.doc(id).update({"rating": rating});
-    res.send("updated song's rating!");
+    else {
+        res.send('Pachinko');
+    }
 });
 
-//STEP 4: DELETE a song 
+//make the GET request with route params
+/*The third endpoint will be a GET request to /book/:userid. This will simply send "userid is really cool!" to the frontend.
 
-app.delete('/deleteSong', async function (req, res) {
-  const id2 = req.query.id;
-  await songs.doc(id2).delete();
-  res.send('deleted song..');
+    Example: a request to /book/peter will send "peter is really cool!" to the frontend.*/
+    
+app.get('/book/:userid', function(req, res) {
+    let name = new String (req.params.userid);
+    res.send(name + ' is very cool!');
 });
+
+
+//set up the server
+app.listen(8080, function(){
+    console.log("listening to the port 8080");
+});
+
